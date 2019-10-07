fe3c05a8587e9e605210503073613519dc03e2f7
Mo_Nazar <44373397+MoNazar@users.noreply.github.com>
2019-10-07 02:32:37 +0200

    Create Neuron.hpp

---
+++ Neuron.hpp
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+class Neuron
+{
+private:
+	static const int NEURON_INPUT_MAX_NUM = 100;
+	static const int NEURON_WEIGHTS_MAX_NUM = 100;
+public:
+	Neuron();
+	~Neuron();
+	Neuron(double input);
+	Neuron(int neurons_input_num);
+
+	double calculate_total_net_input(double* layer_weights, double* neuron_inputs,
+		int neuron_input_num);
+	double calculate_output(double net_inputs);
+	double activite_fun(double net_input);
+	double calculate_activite_fun_diff(double fun_value);
+	double get_neuron_value(double input);
+	void set_neuron_value(double input);
+
+	double neuron_net_input_;
+	double neuron_net_output_;
+
+
+	int neuron_input_num_;
+	double neuron_inputs_[NEURON_INPUT_MAX_NUM];
+	double neuron_weights_[NEURON_WEIGHTS_MAX_NUM];
+	double deltas_;
+};
